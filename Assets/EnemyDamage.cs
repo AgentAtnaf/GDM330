2b65e76cd3a605ca2963194fd59d71c092dce619
AgentAtnaf <111513753+AgentAtnaf@users.noreply.github.com>
2023-08-16 11:34:43 +0700

    Add damge

--- Assets/EnemyDamage.cs
+++ Assets/EnemyDamage.cs
@@ -3,18 +3,24 @@ using UnityEngine;
 public class EnemyDamage : MonoBehaviour
 {
     public int damageAmount = 10; // Amount of damage the enemy deals
+    public float destroyDelay = 1f; // Delay before the enemy self-destructs
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player")) // Change "Player" to the appropriate tag for the player GameObject
         {
-            // Assuming the player has a script for health
             PlayerHealth playerHealth = other.GetComponent<PlayerHealth>();
 
             if (playerHealth != null)
             {
                 playerHealth.TakeDamage(damageAmount); // Deal damage to the player
+                Destroy(gameObject); // Destroy the enemy
             }
         }
+    }
+
+    private void Start()
+    {
+        Destroy(gameObject, destroyDelay); // Destroy the enemy after the specified delay
     }
 }
